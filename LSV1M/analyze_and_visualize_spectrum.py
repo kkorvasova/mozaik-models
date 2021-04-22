3db6bf5f7ab9445e4272718eadaea7924e3d5be5
kkorvasova <karolina.korvasova@mff.cuni.cz>
2021-04-22 12:23:49 +0200

    Agg fix

--- LSV1M/analyze_and_visualize_spectrum.py
+++ LSV1M/analyze_and_visualize_spectrum.py
@@ -1,4 +1,9 @@
 import os
+
+import matplotlib
+if 'DISPLAY' not in os.environ:
+    matplotlib.use('Agg')
+
 import psutil
 import sys
 import mozaik
@@ -18,131 +23,6 @@ logger = mozaik.getMozaikLogger()
 
 process = psutil.Process(os.getpid())
 
-# def spontaneous(data_store, params):
-#     '''
-#     Analyze spectrum during spontaneous activity.
-#     '''
-#
-#     print '\n Starting to analyze spontaneous activity.'
-#
-#     segs = param_filter_query(data_store, sheet_name="V1_Exc_L2/3", ).get_segments()
-#     print 'Loaded altogether {} experiments'.format(len(segs))
-#
-#     specs = {'grating_duration': None,
-#             'sheets': list(set(data_store.sheets()) & set(
-#                 ['V1_Exc_L4', 'V1_Inh_L4', 'V1_Exc_L2/3', 'V1_Inh_L2/3']))
-#             'st_name': 'InternalStimulus'}
-#     segs = param_filter_query(data_store,
-#                     sheet_name="V1_Exc_L2/3",
-#                     st_name=specs['st_name']).get_segments()
-#     print 'Specified {} experiments'. format(len(segs))
-#
-#
-#     popsigs = {}
-#     bs = params['binsize']
-#
-#     for layer in specs['sheets']:
-#
-#         spids = param_filter_query(
-#                                 data_store,
-#                                 sheet_name=layer,
-#                                 st_name=specs['st_name']
-#                                 ).get_segments()[0].get_stored_spike_train_ids()
-#
-#         hists = param_filter_query(data_store,analysis_algorithm='PSTH',
-#                                   y_axis_name='psth (bin={})'.format(bs),
-#                                   sheet_name=layer,
-#                                   st_name=specs['st_name']
-#                                   ).get_analysis_result()
-#         assert len(hists)==1
-#
-#         hist = hists[0]
-#
-#         hist_array = hist.get_asl_by_id(spids)
-#         spar = []
-#         for itm in hist_array:
-#             spar.append(itm.magnitude)
-#
-#         spar = np.squeeze(np.array(spar))
-#
-#         popsigs[layer] = [np.mean(spar, axis=0)]
-#
-#
-#     psds = analysis.calc_psds(popsigs, params)
-#     vs.plot_psds(psds, specs, params)
-#
-#     # # PSDs of neurons, too little spikes
-#     # psds = analysis.calc_unit_psds(data_store, specs, params)
-#     # plotting.plot_psds(psds, specs, params, su=True)
-#
-# def orientations(data_store, params):
-#     '''
-#     Analyze spectrum for different orientations of a full-field grating.
-#     '''
-#
-#     print '\n Starting to analyze FullfieldDriftingSinusoidalGrating.'
-#
-#     specs = {'grating_duration': None,
-#             'sheets':  list(set(data_store.sheets()) & set(
-#                 ['V1_Exc_L4', 'V1_Inh_L4', 'V1_Exc_L2/3', 'V1_Inh_L2/3']))
-#             'st_name': 'FullfieldDriftingSinusoidalGrating'}
-#
-#     segs = param_filter_query(data_store, sheet_name="V1_Exc_L2/3",
-#                                     st_name=specs['st_name']).get_segments()
-#     print 'Specified {} experiments'.format(len(segs))
-#
-#
-#     stimuli = param_filter_query(data_store, sheet_name="V1_Exc_L2/3",
-#                                     st_name=specs['st_name']).get_stimuli()
-#
-#     stimuli_dict=[ast.literal_eval(stimulus) for stimulus in stimuli]
-#     orientations=np.unique(np.array(
-#                     [stimulus['orientation'] for stimulus in stimuli_dict
-#                                     if 'orientation' in stimulus]))
-#     print 'Orientations:', orientations
-#
-#     specs['orientations'] = orientations
-#
-#     popsigs = {}
-#     bs = params['binsize']
-#
-#     for layer in specs['sheets']:
-#
-#         popsigs[layer] = []
-#
-#         for ornr, orientation in enumerate(orientations):
-#             spids = param_filter_query(
-#                                     data_store,
-#                                     sheet_name=layer,
-#                                     st_name=specs['st_name'],
-#                                     st_orientation=specs['orientations'][ornr]
-#                                     ).get_segments()[0].get_stored_spike_train_ids()
-#
-#             hists = param_filter_query(data_store,analysis_algorithm='PSTH',
-#                                       y_axis_name='psth (bin={})'.format(bs),
-#                                       sheet_name=layer,
-#                                       st_name=specs['st_name'],
-#                                       st_orientation=specs['orientations'][ornr]
-#                                       ).get_analysis_result()
-#
-#             ps_orient = []
-#             for hist in hists: # each trial
-#                 hist_array = hist.get_asl_by_id(spids)
-#                 spar = []
-#                 for itm in hist_array: # each neuron
-#                     spar.append(itm.magnitude)
-#
-#                 spar = np.squeeze(np.array(spar))
-#
-#                 ps_orient.append(np.mean(spar, axis=0))
-#
-#             ps_orient = np.array(ps_orient)
-#
-#             popsigs[layer].append(np.mean(ps_orient, axis=0))
-#
-#     psds = analysis.calc_psds(popsigs, params)
-#
-#     vs.plot_psds(psds, specs, params, row_labels=orientations)
 
 
 def perform_analysis_and_visualization(data_store):
